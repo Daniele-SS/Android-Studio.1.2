c41af7cef697514fa6a00bce6d185632e7aa2573
DANIELE SILVA SANTOS <25203622@sn1271249388.local>
2026-09-14 10:12:21 -0300

    first commit

---
+++ app/src/main/java/com/example/componentes/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.componentes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.componentes.ui.theme.ComponentesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComponentesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicComponentsScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicComponentsScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(244, 67, 54, 255))
+    ) {
+            Text(
+                text = "Aulas android",
+                fontSize =  32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF3DDC84)
+            )
+            Text(
+                text = "com Jetpack Compose",
+                fontSize =  16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color(66, 133, 244)
+            )
+    }
+}
